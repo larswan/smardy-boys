b38b525bc6499eec5f1fc2687427591e92a5c308
Larson Collier <collierlarson@gmail.com>
2023-01-22 16:33:57 -0500

    login working good

--- components/SignIn.jsx
+++ components/SignIn.jsx
@@ -1,32 +1,69 @@
-import { Modal, Dimensions, Image, ViewAlert, Pressable, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native';
+import { Alert, Modal, Image, ViewAlert, Pressable, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native';
 import { useState } from 'react';
 import { Button, Icon, Input, LinearGradient, Divider } from '@rneui/themed';
 
+const SignIn = ({navigation}) => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
 
-const SignIn = () => {
-    
-    const screenWidth = Dimensions.get('window').width;
-    const styles = StyleSheet.create({
-        image: {
-            // flex: 1,
-            width: screenWidth * 0.2,
-            aspectRatio: 1
-        },
-        container: {
-            padding: 30,
-            marginTop: 65,
-            alignItems: 'center'
+    const handleSubmit = () => {
+        
+        // add a post request
+        const loginSubmit = async () => {
+            let req = await fetch(``)
+            let res = await req.json()
+            return res
         }
-    })
+        // loginSubmit()
+        
+        // change to if res.ok
+        if(password && username){
+            Alert.alert(`Your ScreenName is ${username} your password is ${password}`)
+            navigation.navigate('TestScreen')
+        }
+    }
 
     return (
-        <View style={styles.container}>
+        <ScrollView containerStyle={styles.container}>
             <Image 
-            style={styles.image} 
-            // className="w-screen h-1/2 justify-center "
+            // style={styles.image} 
+            className="w-full justify-center "
             source={require ("../assets/signInSplash.png")}/>
-        </View>
+            <Input name="username" placeholder='Username' type="text" onChangeText={(username)=>{setUsername(username)}} />
+            <Input name="password" placeholder='Password' type="password" secureTextEntry={true} onChangeText={(password) => {setPassword(password) }} />
+            <Button
+                title="Login"
+                onPress={()=>{handleSubmit()}}
+                buttonStyle={{
+                    borderColor: 'black',
+                }}
+                type="outline"
+                raised
+                titleStyle={{ color: 'black' }}
+                containerStyle={{
+                    width: 200,
+                    marginHorizontal: 50,
+                    marginVertical: 10,
+                }}
+            />
+        </ScrollView>
     )
 }
 export default SignIn
 
+const styles = StyleSheet.create({
+    image: {
+        flex: 1,
+        width: "100%",
+        height: null,
+        objectFit: 'contain',
+        // resizeMode: 'contain'
+    },
+    container: {
+        // padding: 0,
+        paddingBottom: 20,
+        marginTop: 65,
+        width: "100%",
+        alignItems: 'center'
+    }
+})
